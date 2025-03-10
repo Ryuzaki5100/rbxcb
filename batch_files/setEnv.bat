43ba741ba511132da5c9427330207522d8c27a5d
Atharva Gulkotwar <132251284+Ryuzaki5100@users.noreply.github.com>
2025-03-10 11:54:28 +0530

    Refactored rbxcb_3x3x3

--- batch_files/setEnv.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-set GH_OWNER=Ryuzaki5100
-set GH_REPO=devdocs
-set GH_BRANCH=master
---
+++ demo/src/main/java/com/cube/demo/Atharva.java
@@ -0,0 +1,7 @@
+package com.cube.demo;
+
+public class Atharva {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
