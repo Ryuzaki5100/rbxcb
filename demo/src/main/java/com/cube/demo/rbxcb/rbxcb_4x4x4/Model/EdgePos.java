67b4d7a4fb1639a775364417af33cff1fab557f8
Atharva Gulkotwar <132251284+Ryuzaki5100@users.noreply.github.com>
2025-09-05 17:17:14 +0000

    Added simple getters and setters for rbxcb4x4

--- demo/src/main/java/com/cube/demo/rbxcb/rbxcb_4x4x4/Model/EdgePos.java
+++ demo/src/main/java/com/cube/demo/rbxcb/rbxcb_4x4x4/Model/EdgePos.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class EdgePos implements Cloneable {
     private byte[] val;
 
-    public static byte[] SOLVED_VAL = {
+    public static b̦yte[] SOLVED_VAL = {
             0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11,
             12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23
     };
